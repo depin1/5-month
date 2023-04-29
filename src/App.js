225593a9c9c21d3021316eecc2a699fd884d7709
depin1 <boymatobvnusratillo1@gmail.com>
2023-04-29 19:07:08 +0500

    main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
 import './App.css';
-import Lesson52 from './lesson-52/Lesoon52';
-import Practicing from './lesson-52/Forpracticing';
+
 function App() {
 
   return (
     <div className="App">
-     <Practicing/>
+     
     </div>
   );
 }
