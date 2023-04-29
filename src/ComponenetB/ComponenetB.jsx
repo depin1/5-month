7596c9987b74a7d7fba506835df4d4ef367b7a55
depin1 <boymatobvnusratillo1@gmail.com>
2023-04-29 16:49:39 +0500

    main

--- src/ComponenetB/ComponenetB.jsx
+++ src/ComponenetB/ComponenetB.jsx
@@ -6,16 +6,20 @@ function ComponentB() {
 
 const [post,setpost]=useState('posts');
 const [data,setdata]=useState([]);
+const [loading,setloading]=useState(false);
 
 const fatchdata=async ()=>{
 const response=await axios.get(`https://jsonplaceholder.typicode.com/${post}`)
-setdata(response.data)
+setdata(response.data);
+setloading(false)
 console.log(response.data);
 }
 useEffect(()=>{
+setloading(true)
 fatchdata();
 },[post])
-
+if(loading){ return <h2>Loading...</h2> 
+}
     return(
         <div>
             <button onClick={()=>setpost('posts')}>Posts</button>
